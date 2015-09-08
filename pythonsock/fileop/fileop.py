a5cc1b6fccb4c474f40187300aa3f7d3d8a24bcf
onmore <luzhiwei2015@126.com>
2015-09-09 00:15:04 +0800

    ...

--- pythonsock/fileop/fileop.py
+++ pythonsock/fileop/fileop.py
@@ -1,1 +1,16 @@
-im
+import os
+
+
+def listfilepath(pathname):
+    filelist = []
+    tmplist = os.listdir(pathname)
+    for i in tmplist:
+        if os.path.isfile(i)==True:
+            filelist.append(i)
+        else:
+           filelist =  filelist + listfilepath(os.getcwd() + i)
+
+    return filelist
+
+list = listfilepath("D:\MyDrivers")
+print(list)
